07d92361500aaa20c117ad7fc1844155c5358b61
opelkam <martin.opelka@seznam.cz>
2021-12-16 13:41:50 +0100

    main_work_jen_ja

--- Main.py
+++ Main.py
@@ -1,4 +1,10 @@
-# import graphics
+from bs4 import BeautifulSoup as bs
+import requests
+
+response = requests.get()
+
+soup = bs(response.content, 'html.parser')
+
 
 print("high track speed")
 print("vodka no limit")
