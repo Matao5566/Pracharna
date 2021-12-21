be2f832ff9a35a37faf72000eca0a40e81db632e
opelkam <martin.opelka@seznam.cz>
2021-12-21 09:55:07 +0100

    Patch 1.0 Money add fix(finished) balance changes

--- Cookie Clicker/Cookie clicker.py
+++ Cookie Clicker/Cookie clicker.py
@@ -34,15 +34,16 @@ store = pygame.image.load('oven.png')
 pygame.display.set_caption("Cookie Clicker")
 icon = pygame.image.load('cookie.png')
 pygame.display.set_icon(icon)
-
+# multiplier
+multiplier = 1
 # upgrade1
 upgrade1 = 10
 # upgrade2
-upgrade2 = 150
+upgrade2 = 100
 # upgrade3
 upgrade3 = 1000
 # upgrade4
-upgrade4 = 100000
+upgrade4 = 10**6
 # Money
 money = 0
 font = pygame.font.Font('freesansbold.ttf', 32)
@@ -65,25 +66,29 @@ shopY = 80
 shop_icon = pygame.image.load('Shop.png')
 
 
-def Money(i, values):
-    n = 63
-    while moneyadd <= 10 ** n:
-        n -= 3
-        i -= 1
-    if money < 1000000:
+def Money(values):
+    m = 6
+    j = 0
+    while money >= 10 ** m:
+        if m >= 66:
+            break
+        m += 3
+        j += 1
+
+    if money < 10**6:
         money_value = font.render("Cookies : " + str(round(money)), True, (150, 75, 0))
         screen.blit(money_value, (moneyX, moneyY))
-    elif money >= 1000000:
-        money_value = font.render("Cookies : " + str(round(money/10**n, 2)) + values[i], True, (150, 75, 0))
+    elif money >= 10**6:
+        money_value = font.render("Cookies : " + str(round(money/10**(m-3), 2)) + values[j-1], True, (150, 75, 0))
         screen.blit(money_value, (moneyX, moneyY))
-
+    # print(m, j)
 
 def Score():
     if score < 1000000:
-        score_value = font.render("Score : " + str(score), True, (255, 255, 255))
+        score_value = font.render("Score : " + str(score) + "             " + str(round(multiplier*100)) + "%", True, (255, 255, 255))
         screen.blit(score_value, (scoreX, scoreY))
     elif score >= 1000000:
-        score_value = font.render("Score : " + str(score/10**6) + "Mil", True, (255, 255, 255))
+        score_value = font.render("Score : " + str(score/10**6) + "Mil"+ "             " + str(round(multiplier*100)) + "%", True, (255, 255, 255))
         screen.blit(score_value, (scoreX, scoreY))
 
 
@@ -96,57 +101,102 @@ def Shop():
     screen.blit(shop_value, (shopX, shopY))
 
 
-def Moneyadd(i, values):
+def Moneyadd(values):
     n = 63
+    i = 19
     while moneyadd <= 10**n:
         n -= 3
         i -= 1
-    if moneyadd >= 1000000:
-        moneyadd_value = font.render("Cookies per click: " + str(round(moneyadd/10**n, 2)) + values[i], True,
+    if moneyadd >= 10**6:
+        moneyadd_value = font.render("Cookies per click: " + str(round((moneyadd * multiplier)/10**n, 2)) + values[i], True,
                                          (255, 255, 255))
         screen.blit(moneyadd_value, (0, 700))
-    else:
-        moneyadd_value = font.render("Cookies per click: " + str(round(moneyadd)), True,
+    elif moneyadd < 10**6:
+        moneyadd_value = font.render("Cookies per click: " + str(round(moneyadd * multiplier)), True,
                                      (255, 255, 255))
         screen.blit(moneyadd_value, (0, 700))
-
+    # print(n, i)
 
 def Backtobakery():
     bakery_value = font.render("To get back to bakery press <-", True, (255, 255, 255))
     screen.blit(bakery_value, (0, 400))
 
 
-def Upgrade1():
-    upgrade1_value = font.render("<1> Babiččina volba (+1 Cookie per click) Price :" + str(round(upgrade1)), True,
-                                 (0, 255, 0))
-    screen.blit(upgrade1_value, (0, 100))
+def Upgrade1(values):
+    n = 63
+    i = 19
+    while upgrade1 <= 10**n:
+        n -= 3
+        i -= 1
+    if upgrade1 >= 10**6:
+        upgrade1_value = font.render("<1> Babiččina volba (+1 Cookie per click) Price :" + str(round(upgrade1/10**n, 2))
+                                     + values[i], True, (0, 255, 0))
+        screen.blit(upgrade1_value, (0, 100))
+    elif upgrade1 < 10**6:
+        upgrade1_value = font.render("<1> Babiččina volba (+1 Cookie per click) Price :" + str(round(upgrade1)), True,
+                                     (0, 255, 0))
+        screen.blit(upgrade1_value, (0, 100))
 
 
-def Upgrade2():
-    upgrade2_value = font.render("<2> Sušenkové Mlsání (1.5x Cookies per click) Price :" + str(round(upgrade2)), True,
-                                 (0, 255, 0))
-    screen.blit(upgrade2_value, (0, 150))
+
+def Upgrade2(values):
+    n = 63
+    i = 19
+    while upgrade2 <= 10**n:
+        n -= 3
+        i -= 1
+    if upgrade2 >= 10**6:
+        upgrade2_value = font.render("<2> Sušenkové Mlsání (+10 Cookies per click) Price :" + str(round(upgrade2/10**n, 2))
+                                     + values[i], True, (0, 255, 0))
+        screen.blit(upgrade2_value, (0, 150))
+    elif upgrade2 < 10**6:
+        upgrade2_value = font.render("<2> Sušenkové Mlsání (+10 Cookies per click) Price :" + str(round(upgrade2)), True,
+                                     (0, 255, 0))
+        screen.blit(upgrade2_value, (0, 150))
 
 
-def Upgrade3():
-    upgrade3_value = font.render("<3> Mlsný jazýček (+100 Cookies per click) Price :" + str(round(upgrade3)), True,
-                                 (0, 255, 0))
-    screen.blit(upgrade3_value, (0, 200))
+def Upgrade3(values):
+    n = 63
+    i = 19
+    while upgrade3 <= 10**n:
+        n -= 3
+        i -= 1
+    if upgrade3 >= 10**6:
+        upgrade3_value = font.render("<3> Mlsný jazýček (+1% Cookies per click) Price :" + str(round(upgrade3/10**n, 2))
+                                     + values[i], True, (0, 255, 0))
+        screen.blit(upgrade3_value, (0, 200))
+    elif upgrade3 < 10**6:
+        upgrade3_value = font.render("<3> Mlsný jazýček (+1% Cookies per click) Price :" + str(round(upgrade3)), True,
+                                     (0, 255, 0))
+        screen.blit(upgrade3_value, (0, 200))
 
-def Upgrade4():
-    upgrade4_value = font.render("<4> Dvojité sušenkové krabice (2x vaše cookies) Price :" + str(round(upgrade4)),
-                                 True, (0, 255, 0))
-    screen.blit(upgrade4_value, (0, 250))
+
+def Upgrade4(values):
+    m = 6
+    j = 0
+    while upgrade4 >= 10 ** m:
+        if m >= 66:
+            break
+        m += 3
+        j += 1
+    if upgrade4 >= 10**6:
+        upgrade4_value = font.render("<4> Dvojité sušenkové krabice (+50% Cookies per click) Price :" + str(round(upgrade4/10**(m-3), 2))
+                                     + values[j-1], True, (0, 255, 0))
+        screen.blit(upgrade4_value, (0, 250))
+    elif upgrade4 < 10**6:
+        upgrade4_value = font.render("<4> Dvojité sušenkové krabice (+50% Cookies per click) Price :" + str(round(upgrade4)),
+                                     True, (0, 255, 0))
+        screen.blit(upgrade4_value, (0, 250))
 
 
 def update():
     screen.blit(store, (0, 0))
-    Money(i, values)
-    Upgrade1()
-    Upgrade2()
-    Upgrade3()
-    Upgrade4()
-    Moneyadd(i, values)
+    Money(values)
+    Upgrade1(values)
+    Upgrade2(values)
+    Upgrade3(values)
+    Upgrade4(values)
+    Moneyadd(values)
     Score()
     Backtobakery()
     pygame.display.update()
@@ -166,12 +216,13 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 CookieY = 145
-                money += moneyadd
-                celkem += moneyadd
+                money += moneyadd * multiplier
+                celkem += moneyadd * multiplier
                 score += 1
                 data.append([score, money])
                 data2.append([score, celkem])
-                Moneyadd(i, values)
+                Moneyadd(values)
+                Money(values)
             if event.key == pygame.K_RIGHT:
                 screen.blit(store, (0, 0))
                 update()
@@ -182,7 +233,7 @@ while running:
                     if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
                         if money - upgrade1 >= 0:
                             money -= upgrade1
-                            upgrade1 += 10
+                            upgrade1 += 15
                             moneyadd += 1
                             update()
                         else:
@@ -190,24 +241,24 @@ while running:
                     if event.type == pygame.KEYDOWN and event.key == pygame.K_2:
                         if money - upgrade2 >= 0:
                             money -= upgrade2
-                            upgrade2 += (upgrade2/2)
-                            moneyadd += moneyadd
+                            upgrade2 += 200
+                            moneyadd += 10
                             update()
                         else:
                             continue
                     if event.type == pygame.KEYDOWN and event.key == pygame.K_3:
                         if money - upgrade3 >= 0:
                             money -= upgrade3
-                            upgrade3 += upgrade3
-                            moneyadd += 100
+                            upgrade3 += 1.25*upgrade3
+                            multiplier += 0.01
                             update()
                         else:
                             continue
                     if event.type == pygame.KEYDOWN and event.key == pygame.K_4:
                         if money - upgrade4 >= 0:
                             money -= upgrade4
-                            upgrade4 += upgrade4 * 2
-                            moneyadd += money
+                            upgrade4 += upgrade4 * 1.5
+                            multiplier += 0.5
                             update()
                         else:
                             continue
@@ -252,7 +303,7 @@ while running:
                 CookieY = 150
     Cookie()
     Shop()
-    Money(i, values)
+    Money(values)
     Score()
-    Moneyadd(i, values)
+    Moneyadd(values)
     pygame.display.update()
